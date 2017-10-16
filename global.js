a5a4b1626fb332df5120f5c3806ae52371306c96
peanut <peanut>
2017-10-16 19:58:54 +0700

    undone

--- global.js
+++ global.js
@@ -122,68 +122,78 @@ moderate: [{
           "correct": "decreases"
         }],
   hard: [{
-          "question": "UVWEWVWEVWEVWEV",
-          "choices": ["Internet Provider", "Internet Port", "Internet Protocol"],
-          "correct": "Internet Protocol"
+          "question": "Tanya is older than Eric, Cliff is older than Tanya. Eric is older than Cliff. If the first two statements are true, the third statement is",
+          "choices": ["true", "false", "Iuncertain"],
+          "correct": "false"
         }, {
-          "question": "Who is the founder of Microsoft?",
-          "choices": ["Bill Gates", "Steve Jobs", "Steve Wozniak"],
-          "correct": "Bill Gates"
+          "question": "Blueberries cost more than strawberries. Blueberries cost less than raspberries. Raspberries cost more than strawberries and blueberries. If the first two statements are true, the third statement is",
+          "choices": ["true", "false", "uncertain"],
+          "correct": "true"
         }, {
-          "question": "1 byte = ?",
-          "choices": ["8 bits", "64 bits", "1024 bits"],
-          "correct": "8 bits"
+          "question": "All the trees in the park are flowering trees. Some of the trees in the park are dogwoods. All dogwoods in the park are flowering trees. If the first two statements are true, the third statement is",
+          "choices": ["true", "false", "uncertain"],
+          "correct": "true"
         }, {
-          "question": "The C programming language was developed by?",
-          "choices": ["Brendan Eich", "Dennis Ritchie", "Guido van Rossum"],
-          "correct": "Dennis Ritchie"
+          "question": "Mara runs faster than Gail. Lily runs faster than Mara. Gail runs faster than Lily. If the first two statements are true, the third statement is",
+          "choices": ["true", "false", "uncertain"],
+          "correct": "false"
         }, {
-          "question": "What does CC mean in emails?",
-          "choices": ["Carbon Copy", "Creative Commons", "other"],
-          "correct": "Carbon Copy"
+          "question": "Apartments in the Riverdale Manor cost less than apartments in The Gaslight Commons. Apartments in the Livingston Gate cost more than apartments in the The Gaslight Commons. Of the three apartment buildings, the Livingston Gate costs the most. If the first two statements are true, the third statement is",
+          "choices": ["true", "false", "uncertain"],
+          "correct": "true"
         }, {
-          "question": "Who is the founder of Microsoft?",
-          "choices": ["Bill Gates", "Steve Jobs", "Steve Wozniak"],
-          "correct": "Bill Gates"
+          "question": "Class A has a higher enrollment than Class B. Class C has a lower enrollment than Class B. Class A has a lower enrollment than Class C. If the first two statements are true, the third statement is",
+          "choices": ["true", "false", "uncertain"],
+          "correct": "false"
         }, {
-          "question": "1 byte = ?",
-          "choices": ["8 bits", "64 bits", "1024 bits"],
-          "correct": "8 bits"
+          "question": "Four people witnessed a mugging. Each gave a different description of the mugger. Which description is probably right?",
+          "choices": ["He was average height, thin, and middle-aged", "He was tall, thin, and middle-aged", "He was tall, thin, and young"],
+          "correct": "He was tall, thin, and middle-aged"
         }, {
-          "question": "The C programming language was developed by?",
-          "choices": ["Brendan Eich", "Dennis Ritchie", "Guido van Rossum"],
-          "correct": "Dennis Ritchie"
+          "question": "Ms. Forest likes to let her students choose who their partners will be; however, no pair of students may work together more than seven class periods in a row. Adam and Baxter have studied together seven class periods in a row. Carter and Dennis have worked together three class periods in a row. Carter does not want to work with Adam. Who should be assigned to work with Baxter?",
+          "choices": ["Carter", "Adam", "Dennis"],
+          "correct": "Carter"
         }, {
-          "question": "What does CC mean in emails?",
-          "choices": ["Carbon Copy", "Creative Commons", "other"],
-          "correct": "Carbon Copy"
+          "question": "At the baseball game, Henry was sitting in seat 253. Marla was sitting to the right of Henry in seat 254. In the seat to the left of Henry was George. Inez was sitting to the left of George. Which seat is Inez sitting in?",
+          "choices": ["251", "255", "256"],
+          "correct": "251"
         }, {
-          "question": "Who is the founder of Microsoft?",
-          "choices": ["Bill Gates", "Steve Jobs", "Steve Wozniak"],
-          "correct": "Bill Gates"
+          "question": "As they prepare for the state championships, one gymnast must be moved from the Level 2 team to the Level 1 team. The coaches will move the gymnast who has won the biggest prize and who has the most experience. In the last competition, Roberta won a bronze medal and has competed seven times before. Jamie has won a silver medal and has competed fewer times than Roberta. Beth has won a higher medal than Jamie and has competed more times than Roberta. Michele has won a bronze medal, and it is her third time competing. Who will be moved to the Level 1 team?",
+          "choices": ["Roberta", "Beth", "Michele"],
+          "correct": "Beth"
         }, {
-          "question": "1 byte = ?",
-          "choices": ["8 bits", "64 bits", "1024 bits"],
-          "correct": "8 bits"
+          "question": "Four friends in the sixth grade were sharing a pizza. They decided that the oldest friend would get the extra piece. Randy is two months older than Greg, who is three months younger than Ned. Kent is one month older than Greg. Who should get the extra piece of pizza?",
+          "choices": ["Randy", "Greg", "Ned"],
+          "correct": "Ned"
         }, {
-          "question": "The C programming language was developed by?",
-          "choices": ["Brendan Eich", "Dennis Ritchie", "Guido van Rossum"],
-          "correct": "Dennis Ritchie"
+          "question": "The high school math department needs to appoint a new chairperson, which will be based on seniority. Ms. West has less seniority than Mr. Temple, but more than Ms. Brody. Mr. Rhodes has more seniority than Ms. West, but less than Mr. Temple. Mr. Temple doesn't want the job. Who will be the new math department chairperson?",
+          "choices": ["Mr. Rhodes", "Mr. Temple", "Ms.West"],
+          "correct": "Mr. Rhodes"
         }, {
-          "question": "What does CC mean in emails?",
-          "choices": ["Carbon Copy", "Creative Commons", "other"],
-          "correct": "Carbon Copy"
+          "question": "Danielle has been visiting friends in Ridge-wood for the past two weeks. She is leaving tomorrow morning and her flight is very early. Most of her friends live fairly close to the airport. Madison lives ten miles away. Frances lives five miles away, Samantha, seven miles. Alexis is farther away than Frances, but closer than Samantha. Approximately how far away from the airport is Alexis?",
+          "choices": ["nine miles", "eight miles", "six miles"],
+          "correct": "six miles"
         }, {
-          "question": "The C programming language was developed by?",
-          "choices": ["Brendan Eich", "Dennis Ritchie", "Guido van Rossum"],
-          "correct": "Dennis Ritchie"
+          "question": "Nurse Kemp has worked more night shifts in a row than Nurse Rogers, who has worked five. Nurse Miller has worked fifteen night shifts in a row, more than Nurses Kemp and Rogers combined. Nurse Calvin has worked eight night shifts in a row, less than Nurse Kemp. How many night shifts in a row has Nurse Kemp worked?",
+          "choices": ["eight", "nine", "ten"],
+          "correct": "nine"
         }, {
-          "question": "What does CC mean in emails?",
-          "choices": ["Carbon Copy", "Creative Commons", "other"],
-          "correct": "Carbon Copy"
+          "question": "Children are in pursuit of a dog whose leash has broken. James is directly behind the dog. Ruby is behind James. Rachel is behind Ruby. Max is ahead of the dog walking down the street in the opposite direction. As the children and dog pass, Max turns around and joins the pursuit. He runs in behind Ruby. James runs faster and is alongside the dog on the left. Ruby runs faster and is alongside the dog on the right. Which child is directly behind the dog?",
+          "choices": ["Rachel", "Ruby", "Max"],
+          "correct": "Max"
         }],
 };
+var score = {
+  easy:0,
+  moderate:0,
+  hard:0
+};
 
+var level = {
+  easy:0,
+  moderate:0,
+  hard: 0
+};
 
 // define elements
 var content = $("content"),
@@ -194,19 +204,23 @@ var content = $("content"),
 
 // init vars
 var currentQuestion = 0,
-  score = 0,
   askingQuestion = true;
 
 var diff;
 var choices;
 var choicesHtml;
+
+
     
 function $(id) { // shortcut for document.getElementById
   return document.getElementById(id);
 }
 
 function askQuestion() {
+  
+  console.log("inside askquestion");
   choices = quiz[diff][currentQuestion].choices;
+  // console.log(choices);
   choicesHtml = "";
  
   // loop through choices, and create radio buttons
@@ -217,9 +231,10 @@ function askQuestion() {
       " <label for='choice" + (i + 1) + "'>" + choices[i] + "</label></span><br><br>";
 
   }
-
+  console.log('quizlength '+ quiz[diff].length);
+  console.log('question ' + currentQuestion);
   // load the question
-  questionContainer.textContent = "Q" + (currentQuestion + 1) + ". " +
+  questionContainer.textContent = 
     quiz[diff][currentQuestion].question;
 
   // load the choices
@@ -229,11 +244,13 @@ function askQuestion() {
   if (currentQuestion === 0) {
     scoreContainer.textContent = "Score: 0 right answers out of " +
       quiz[diff].length + " possible.";
+    scoreContainer.textContent = "NOTE:Leaving question will be no points";
     submitBtn.textContent = "Submit Answer";
   }
 }
 
 function checkAnswer() {
+  console.log('inside check answer');
   // are we asking a question, or proceeding to next question?
   if (askingQuestion) {
     submitBtn.textContent = "Next Question";
@@ -258,21 +275,22 @@ function checkAnswer() {
     var labelStyle = document.getElementsByTagName("label")[correctIndex].style;
     labelStyle.fontWeight = "bold";
     if (userpick == quiz[diff][currentQuestion].correct) {
-      score++;
+      score[diff]++;
       // labelStyle.color = "green";
     }
     //  else {
     //   labelStyle.color = "white";
     // }
-
-    scoreContainer.textContent = "Score: " + score + " right answers out of " +
+    scoreContainer.textContent = "Score: 0 right answers out of " +
       quiz[diff].length + " possible.";
+    scoreContainer.textContent = "NOTE:Leaving question will be no points";
   } else { // move to next question
     // setting up so user can ask a question
     askingQuestion = true;
     // change button text back to "Submit Answer"
     submitBtn.textContent = "Submit Answer";
     // if we're not on last question, increase question number
+    console.log(currentQuestion < quiz[diff].length - 1);
     if (currentQuestion < quiz[diff].length - 1) {
       currentQuestion++;
       askQuestion();
@@ -280,14 +298,15 @@ function checkAnswer() {
       showFinalResults();
     }
 
+
   }
 }
 
 function showFinalResults() {
   content.innerHTML = "<h2>You've completed the quiz!</h2>" +
-    "<h2>Below are your results:</h2>" +
-    "<h2>" + score + " out of " + quiz[diff].length + " questions, " +
-    Math.round(score / quiz[diff].length * 100) + "%<h2>";
+    "<h2>" + score[diff] + " out of " + quiz[diff].length + " questions, " +
+    Math.round(score[diff] / quiz[diff].length * 100) + "%<h2>" +
+     "<button class='get'>Choose Again</button>";
 }
 
 // window.addEventListener("load", askQuestion, false);
--- jquery.main.js
+++ jquery.main.js
@@ -6,22 +6,29 @@ $(document).ready(function(){
 	});
     $('#content').hide();
     $('#pick').hide();
-    $('.landing-page').ready(function(){
-        $('.landing-page').show();
-        $('.difficulty').hide();
-        $('.container').hide();
-    });
+    // $('.landing-page').ready(function(){
+    //     $('.landing-page').show();
+    //     $('.difficulty').hide();
+    //     $('.container').hide();
+    // });
     $('#play').click(function(){
     	$('#pick').slideDown('slow');
     	$('#play').show();
     	$('#content').hide();
     });
     $('.choose').click(function(){
+        console.log("Fire");
         $('#content').slideDown(2000);
         $('#question').fadeIn(2000);
         $('#choices').fadeIn(2000);
     	$('#play').hide();
         $('#pick').hide();
     });
-   
+    $('#content').on('click','.get',function(){
+        currentQuestion = 0;
+        $('#pick').slideDown('slow');
+        $('#play').show();
+        $('#content').hide();
+    });
+    $('.get').css('padding', '10px');
 });
